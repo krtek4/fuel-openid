9bd6211916d56578ae97e2e8b57acf12f69c926b
Gilles Meier <krtek4@gmail.com>
2011-08-17 11:43:04 -0400

    added an OpenID Selector view to the package, controller improvement, now return an error when the provided doesn't respond

---
+++ classes/controller/selector.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * @package    openid
+ * @version    1.0
+ * @license    GPLv3
+ * @author     Gilles Meier
+ * @link       http://github.com/krtek4/fuel-openid
+ */
+
+class OpenID_Selector extends \Fuel\Core\Singleton {
+	/**
+	 * Return the HTML for an OpenId Selector form. You must provide the URL the provided must
+	 * call after authenticating the user. If you're using the provided controller it will be the login action.
+	 *
+	 * @param string $url the URL for the provider
+	 * @return string HTML form for OpenID provider selection
+	 */
+	public function get_form($url) {
+		return \View::factory('form')->set('url', $url);
+	}
+
+	public function get_js() {
+		return 'openid.init("openid_identifier");';
+	}
+}
+?>
